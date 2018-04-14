cff25f2e4df9bd8921bb071ea7282c47269584aa
gnn06 <gilles.orsini@gmail.com>
2018-04-14 14:40:45 +0200

    rajout suppression tache refactor UI (non finalisé)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-get 'plan',                   to: 'plan#index'
-get 'plan/:id/new',           to: 'plan#new'
-get 'issues/:id/plan',        to: 'plan#show', as: 'edit_plan'
-get 'issues/:id/plan/new',    to: 'plan#subtask', as: 'subtask'
+get    'plan',                   to: 'plan#index'
+get    'plan/:id/new',           to: 'plan#new'
+get    'issues/:id/plan',        to: 'plan#show',    as: 'edit_plan'
+get    'issues/:id/plan/new',    to: 'plan#subtask', as: 'subtask'
