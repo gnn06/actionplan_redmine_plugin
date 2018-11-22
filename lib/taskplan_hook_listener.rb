e4d5bd0d7a8acd39a62960a1f4ef7ceab0bcfe49
gilles orsini <gilles.orsini@gmail.com>
2018-11-22 22:45:18 +0100

    back_url sur action new_task vers issues_list

--- lib/taskplan_hook_listener.rb
+++ lib/taskplan_hook_listener.rb
@@ -1,15 +1,12 @@
-require 'byebug'
-
 class TaskplanHookListener < Redmine::Hook::ViewListener
   include IssuesHelper
     def view_issues_context_menu_start(context = {})
-      #byebug
       issue = context[:issues][0]
       attrs = {
         :parent_issue_id => issue
       }
       attrs[:tracker_id] = issue.tracker unless issue.tracker.disabled_core_fields.include?('parent_issue_id')
-      l = link_to("new subtask", new_project_issue_path(issue.project, :issue => attrs, :back_url => issue_path(issue)))
+      l = link_to("new subtask", new_project_issue_path(issue.project, :issue => attrs, :back_url => project_issues_path(issue.project)))
       return "<li>" + l + "</li>"
     end  
   end
