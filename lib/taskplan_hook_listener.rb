358301cce0d3a0cae7dc1b3f8bb1b97ea1744fe7
gilles orsini <gilles.orsini@gmail.com>
2018-12-02 22:14:44 +0100

    keep current filters

--- lib/taskplan_hook_listener.rb
+++ lib/taskplan_hook_listener.rb
@@ -13,23 +13,42 @@ class TaskplanHookListener < Redmine::Hook::ViewListener
     # create subtask
     l1 = link_to("new subtask", new_project_issue_path(issue.project, :issue => attrs, :back_url => project_issues_path(issue.project)))
     # byebug
-    # TODO maintenir columns, order
     # filter subtasks
+    # TODO maintenir columns, order
+    # TODO manage saved query
+    current_filters = context[:request].session[:issue_query][:filters]
+    filters, operators, values = url_filters current_filters
     l2 = link_to("filter sustasks", _project_issues_path(@project, :set_filter => 1,
-      :f => ["parent_id", ""], 
-      :op => {:parent_id => "~"},
-      :v => {:parent_id => [issue.id]},
-      :issue_id => ["1"]))
+      :f  => filters << "parent_id", 
+      :op => operators.merge({"parent_id" => "~"}),
+      :v  => values.merge({"parent_id" => [issue.id.to_s]})))
     # filter parent task 
-    # byebug
     # TODO manage no grand_parent
     direct_parent_id = issue.parent.parent_id
+    # l3 = link_to("filter parent tasks", _project_issues_path(@project, :set_filter => 1,
+    #   :f => ["parent_id", "status"], 
+    #   :op => [{:parent_id => "~"}, 
+    #   :v => {:parent_id => [direct_parent_id]}
+    # ))
+    # l3 = link_to("filter parent tasks", project_issues_path(issue.project))
     l3 = link_to("filter parent tasks", _project_issues_path(@project, :set_filter => 1,
-    :f => ["parent_id", ""], 
-    :op => {:parent_id => "~"},
-    :v => {:parent_id => [direct_parent_id]}))
+      :f  => filters << "parent_id",
+      :op => operators.merge({"parent_id" => "~"}),
+      :v  => values.merge({"parent_id" => [direct_parent_id.to_s]})))
     return "<li>" + l1 + "</li>" +
           "<li>" + l2 + "</li>" +
-          "<li>" + l3.to_s+ "</li>"
-  end  
+          "<li>" + l3 + "</li>"
+  end
+
+  def url_filters(filters_hash)
+    filters = []
+    operators = {}
+    values = {}
+    filters_hash.each do |key, value|
+      filters << key
+      operators[key] = value[:operator]
+      values[key] = value[:values]
+    end
+    return filters, operators, values
+  end
 end
